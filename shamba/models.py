2bf5d801e8a353207b8413682db8cc5c84b84889
Dennis Kamunya <dennismuriithik@gmail.com>
2020-11-03 16:32:04 +0300

    Create shamba post comment model

--- shamba/models.py
+++ shamba/models.py
@@ -85,3 +85,15 @@ class Post(models.Model):
         return self.name 
 
 
+class Post_Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User,  on_delete=models.CASCADE)
+    comment = models.CharField(max_length=140)
+    comment_date = models.DateTimeField(auto_now_add=True)
+
+
+
+    def __str__(self):
+      return self.id         
+
+
