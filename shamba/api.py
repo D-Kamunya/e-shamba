9decb35c28e48fc1350b53c6a21e2d5aeac73512
Dennis Kamunya <dennismuriithik@gmail.com>
2020-11-04 12:40:35 +0300

    Add permissions to crop api endpoints

--- shamba/api.py
+++ shamba/api.py
@@ -2,26 +2,38 @@ from rest_framework.response import Response
 from rest_framework import generics
 from .serializer import CropSerializer
 from .models import Crop
-
+from rest_framework import generics, permissions
 # Crop Create API
 class CropCreateApi(generics.CreateAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
     queryset = Crop.objects.all()
     serializer_class = CropSerializer
 
 
 # Crop List API
 class CropApi(generics.ListAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
     queryset = Crop.objects.all()
     serializer_class = CropSerializer
 
 
 class CropDetailApi(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
     queryset = Crop.objects.all()
     serializer_class = CropSerializer
 
 
 
 class UsersCropListApi(generics.ListAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
     serializer_class = CropSerializer
 
     def get_queryset(self):
