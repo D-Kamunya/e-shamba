1ca7a38973dd4b7b23b1dd42383d235547a951d7
Dennis Kamunya <dennismuriithik@gmail.com>
2020-11-04 12:05:48 +0300

    Create user serializer and nest it in crop

--- shamba/serializer.py
+++ shamba/serializer.py
@@ -1,6 +1,16 @@
 from rest_framework import  serializers
 from .models import Crop
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id','first_name', 'last_name', 'username', 'email')
+
+
 class CropSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False)
     class Meta:
         model = Crop
         fields = '__all__'
