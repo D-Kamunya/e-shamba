6f98b4c2750373706a124a3baeccc897d13b908a
Dennis Kamunya <dennismuriithik@gmail.com>
2020-11-04 12:03:12 +0300

    Craete shamba app crop api endpoints

--- shamba/api.py
+++ shamba/api.py
@@ -1,1 +1,20 @@
 from rest_framework.response import Response
+from rest_framework import generics
+from .serializer import CropSerializer
+from .models import Crop
+
+# Crop Create API
+class CropCreateApi(generics.CreateAPIView):
+    queryset = Crop.objects.all()
+    serializer_class = CropSerializer
+
+
+# Crop List API
+class CropApi(generics.ListAPIView):
+    queryset = Crop.objects.all()
+    serializer_class = CropSerializer
+
+
+class CropDetailApi(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Crop.objects.all()
+    serializer_class = CropSerializer
