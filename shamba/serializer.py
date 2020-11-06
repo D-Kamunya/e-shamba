08525642215ab68d3b5ae5c032fecf1b732aa56b
Dennis Kamunya <dennismuriithik@gmail.com>
2020-11-06 08:07:40 +0300

    Edit crop serializer

--- shamba/serializer.py
+++ shamba/serializer.py
@@ -15,7 +15,6 @@ class ScheduleSerializer(serializers.ModelSerializer):
 
 
 class CropSerializer(serializers.ModelSerializer):
-    user = UserSerializer(many=False)
     class Meta:
         model = Crop
         fields = '__all__'
